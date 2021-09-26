fa8ab09a04a332bc3f6e7765438a5b256963bb4a
Nicholas Barfknecht <nicholas@nml.co.za>
2021-09-26 16:28:28 +0200

    build: trigger action

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,5 +2,5 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-    return (<div><h1>Container Test</h1><hr/><MarketingApp/></div>);
+    return (<div><h1>Container</h1><hr/><MarketingApp/></div>);
 };
